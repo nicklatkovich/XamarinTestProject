faa050dd4f5818f5be679443869a4657bc145ba2
NickLatkovich <nicklatkovich@gmail.com>
2017-10-09 19:39:13 +0300

    Header logo and margin Fix

--- XamarinTestProject/XamarinTestProject/MainPage.xaml.cs
+++ XamarinTestProject/XamarinTestProject/MainPage.xaml.cs
@@ -14,6 +14,7 @@ namespace XamarinTestProject {
             PhoneIcon.Source = ImageSource.FromResource("XamarinTestProject.Images.phoneIcon.png");
             CameraIcon.Source = ImageSource.FromResource("XamarinTestProject.Images.cameraIcon.png");
             ExplorerIcon.Source = ImageSource.FromResource("XamarinTestProject.Images.explorerIcon.png");
+            Header.Source = ImageSource.FromResource("XamarinTestProject.Images.mainHeaderLogo.png");
         }
     }
 }
